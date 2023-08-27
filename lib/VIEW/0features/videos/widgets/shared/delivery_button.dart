2cfc66ab5bd2854140033046bd18a6a9422a5a96
RonaldTaD <ronaldaparicio568@gmail.com>
2023-08-27 15:29:49 -0400

    Importacion Obsoleta

--- lib/VIEW/0features/videos/widgets/shared/delivery_button.dart
+++ lib/VIEW/0features/videos/widgets/shared/delivery_button.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:animate_do/animate_do.dart';
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:provechopolis/global_responsive.dart';
 
