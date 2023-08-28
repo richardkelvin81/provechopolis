42cbb9944550851339a7dbbb2471d6fa63dc6314
RonaldTaD <ronaldaparicio568@gmail.com>
2023-08-28 17:23:42 -0400

    Reajuste de tamaño del titulo

--- lib/VIEW/0features/videos/widgets/shared/video_titulos.dart
+++ lib/VIEW/0features/videos/widgets/shared/video_titulos.dart
@@ -42,7 +42,7 @@ class VideoTitulos extends StatelessWidget {
               style: TextStyle(
                   fontFamily: 'Barlow Bold',
                   color: Colors.white,
-                  fontSize: GlobalResponsive.smallFont(context) + 2.5),
+                  fontSize: GlobalResponsive.smallFont(context) ),
               ),
           ),
           
