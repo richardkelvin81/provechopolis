ab9ca326ec9de6a77ba0df08271b17331ec5b080
RonaldTaD <ronaldaparicio568@gmail.com>
2023-09-13 22:01:00 -0400

    Preparando archivo para S3 & CloudFront

--- lib/VIEW/0features/crear/crear_video.dart
+++ lib/VIEW/0features/crear/crear_video.dart
@@ -14,30 +14,29 @@ class CrearVideo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CupertinoPageScaffold(
-        child: NestedScrollView(
-            headerSliverBuilder: ((context, innerBoxIsScrolled) => [
-              CupertinoSliverNavigationBar(
-                backgroundColor: Colors.transparent,
-                largeTitle:const Text(
-                  "Crear",
-                  style: TextStyle(color: Colors.black),
-                ),
-                leading: CupertinoButton(
-                  padding: EdgeInsets.zero,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Icon(
-                      Icons.arrow_back_ios, 
-                      color: Color(0xFFF8C358),)),
-                  )
-                ]),
-            body: const InstanciaCrearVideo()
-          )),
+          child: NestedScrollView(
+              headerSliverBuilder: ((context, innerBoxIsScrolled) => [
+                    CupertinoSliverNavigationBar(
+                      backgroundColor: Colors.transparent,
+                      largeTitle: const Text(
+                        "Crear",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      leading: CupertinoButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.arrow_back_ios,
+                            color: Color(0xFFF8C358),
+                          )),
+                    )
+                  ]),
+              body: const InstanciaCrearVideo())),
     );
   }
 }
-
 
 class InstanciaCrearVideo extends StatefulWidget {
   const InstanciaCrearVideo({super.key});
@@ -47,19 +46,20 @@ class InstanciaCrearVideo extends StatefulWidget {
 }
 
 class _InstanciaCrearVideoState extends State<InstanciaCrearVideo> {
-    XFile? _videoFile;
-    final FlutterFFmpeg _flutterFFmpeg = FlutterFFmpeg();
-    
+  XFile? _videoFile;
+  final FlutterFFmpeg _flutterFFmpeg = FlutterFFmpeg();
 
   Future<void> _pickVideoFromCamera() async {
-    XFile? pickedVideo = await ImagePicker.platform.getVideo(source: ImageSource.camera);
+    XFile? pickedVideo =
+        await ImagePicker.platform.getVideo(source: ImageSource.camera);
     setState(() {
       _videoFile = pickedVideo;
     });
   }
 
   Future<void> _pickVideoFromGallery() async {
-    XFile? pickedVideo = await ImagePicker.platform.getVideo(source: ImageSource.gallery);
+    XFile? pickedVideo =
+        await ImagePicker.platform.getVideo(source: ImageSource.gallery);
     setState(() {
       _videoFile = pickedVideo;
     });
@@ -70,19 +70,16 @@ class _InstanciaCrearVideoState extends State<InstanciaCrearVideo> {
       return;
     }
 
-
-          
-      final compressedVideoPath = await VideoCompress.compressVideo(
+    final compressedVideoPath = await VideoCompress.compressVideo(
       _videoFile!.path,
       quality: VideoQuality.DefaultQuality,
       deleteOrigin: false,
     );
 
-     final outputVideoPath =
-        _videoFile!.path.replaceAll('.mp4', '.webm');
+    final outputVideoPath = _videoFile!.path.replaceAll('.mp4', '.webm');
 
-      // Proxima Refactorizacion de Integracion AWS S3 & CloudFront  
-  /*
+    // Proxima Refactorizacion de Integracion AWS S3 & CloudFront
+    /*
   final FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg ();
 
     _flutterFFmpeg.execute ("-i $compressedVideoPath -c:v libvpx-vp9 -b:v 0 -c:a libopus $outputVideoPath").then((rc) {
@@ -108,72 +105,68 @@ class _InstanciaCrearVideoState extends State<InstanciaCrearVideo> {
 
 
     });*/
-          
 
-   
-  
-  
-   
-
-  //  await uploadTask.whenComplete(() {
-  //    print('Video uploaded');
-  //  });
+    //  await uploadTask.whenComplete(() {
+    //    print('Video uploaded');
+    //  });
   }
 
-  Future<void> compressAndConvertToWebM(String inputPath, String outputPath) async {
-  //  ffmpeg -i input_file.mp4 -c:v livpx -crf 10 -b:v 1M -c:a libvorbis output_file.webm
-  final arguments = [
-    '-i', inputPath,
-    '-c:v', 'libvpx',
-    '-crf', 10,
-    '-b:v', '1M',
-    '-c:a', 'libopus',
-    outputPath
-  ];
+  Future<void> compressAndConvertToWebM(
+      String inputPath, String outputPath) async {
+    //  ffmpeg -i input_file.mp4 -c:v livpx -crf 10 -b:v 1M -c:a libvorbis output_file.webm
+    final arguments = [
+      '-i',
+      inputPath,
+      '-c:v',
+      'libvpx',
+      '-crf',
+      10,
+      '-b:v',
+      '1M',
+      '-c:a',
+      'libopus',
+      outputPath
+    ];
 
-  int result = await _flutterFFmpeg.executeWithArguments(arguments);
+    int result = await _flutterFFmpeg.executeWithArguments(arguments);
 
-  if (result == 0) {
-    print('Compresión y conversión a WebM exitosas');
-    
-  } else {
-    print('Error en la compresión y conversión a WebM');
+    if (result == 0) {
+      print('Compresión y conversión a WebM exitosas');
+    } else {
+      print('Error en la compresión y conversión a WebM');
+    }
   }
-}
-
-
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _videoFile != null
-                ? VideoPlayerWidget(videoFile: File(_videoFile!.path))
-                : Text('No video selected'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickVideoFromCamera,
-              child: Text('Record Video'),
-            ),
-            ElevatedButton(
-              onPressed: _pickVideoFromGallery,
-              child: Text('Select Video from Gallery'),
-            ),
-            ElevatedButton(
-              onPressed: _uploadVideo,
-              child: Text('Upload Video to Firebase'),
-            ),
-          ],
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _videoFile != null
+              ? VideoPlayerWidget(videoFile: File(_videoFile!.path))
+              : Text('No video selected'),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _pickVideoFromCamera,
+            child: Text('Record Video'),
+          ),
+          ElevatedButton(
+            onPressed: _pickVideoFromGallery,
+            child: Text('Select Video from Gallery'),
+          ),
+          ElevatedButton(
+            onPressed: _uploadVideo,
+            child: Text('Upload Video to Firebase'),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class VideoPlayerWidget extends StatelessWidget {
   final File videoFile;
- 
 
   const VideoPlayerWidget({super.key, required this.videoFile});
 
